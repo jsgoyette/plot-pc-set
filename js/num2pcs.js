19c99ab18d3db5ca1659dd9381eef0b1cbaae969
Jeremiah Goyette <jsgoyette@gmail.com>
2012-05-27 11:28:23 -0400

    converted tabs to spaces

--- js/num2pcs.js
+++ js/num2pcs.js
@@ -1,40 +1,40 @@
 var notenames = {'g##':9, 'a':9, 'bbb':9, 'a#':10, 'bb':10, 'a##':11, 
-		'b':11, 'cb':11, 'b#':0, 'c':0, 'dbb': 0, 'c#':1, 'db':1, 'c##':2, 
-		'd':2, 'd#':3, 'eb':3, 'e':4, 'fb':4, 'e#':5, 'f':5, 'gbb':5,
-		'f#':6, 'gb':6, 'f##':7, 'g':7, 'abb':7, 'g#':8, 'ab':8};
+    'b':11, 'cb':11, 'b#':0, 'c':0, 'dbb': 0, 'c#':1, 'db':1, 'c##':2, 
+    'd':2, 'd#':3, 'eb':3, 'e':4, 'fb':4, 'e#':5, 'f':5, 'gbb':5,
+    'f#':6, 'gb':6, 'f##':7, 'g':7, 'abb':7, 'g#':8, 'ab':8};
 
 var compressed = false;
 function numOrdA(a, b){ return (a-b); }
 
 function pcstr2pcs(mod, pstr) {
-	var arr,
-	    val = 0,
-	    outarr = [];
-	pstr = pstr.replace(/,/g," ");
-	pstr = pstr.replace(/_/g," ");
-	if (compressed) pstr = pstr.replace(/(.)/g,'$1 ')
-	pstr = pstr.replace(/^\s+|\s+$/g,'').replace(/\s+/g,' ');
-	if (pstr == "") return [];
-	arr = pstr.split(' ')
-	for (x in arr) {
-		if (mod == 12 && !compressed && notenames[arr[x].toLowerCase()] != undefined) {
-			arr[x] = notenames[arr[x].toLowerCase()];
-		}
-		if (mod == 12 && compressed) {
-			if (arr[x].toLowerCase() == 't') arr[x] = 10;
-			if (arr[x].toLowerCase() == 'e') arr[x] = 11;
-		}
-	  if (!isNaN(arr[x])) { 
-	    outarr.push(arr[x]); 
-	  }
-	}
-	for (x in outarr) {
-	  outarr[x] = Math.floor(outarr[x]);
-	  while (outarr[x]*1 >= mod) 
-		outarr[x] -= mod;
-	}
-	outarr = unique(outarr);
-	return outarr;
+  var arr,
+      val = 0,
+      outarr = [];
+  pstr = pstr.replace(/,/g," ");
+  pstr = pstr.replace(/_/g," ");
+  if (compressed) pstr = pstr.replace(/(.)/g,'$1 ')
+  pstr = pstr.replace(/^\s+|\s+$/g,'').replace(/\s+/g,' ');
+  if (pstr == "") return [];
+  arr = pstr.split(' ')
+  for (x in arr) {
+    if (mod == 12 && !compressed && notenames[arr[x].toLowerCase()] != undefined) {
+      arr[x] = notenames[arr[x].toLowerCase()];
+    }
+    if (mod == 12 && compressed) {
+      if (arr[x].toLowerCase() == 't') arr[x] = 10;
+      if (arr[x].toLowerCase() == 'e') arr[x] = 11;
+    }
+    if (!isNaN(arr[x])) { 
+      outarr.push(arr[x]); 
+    }
+  }
+  for (x in outarr) {
+    outarr[x] = Math.floor(outarr[x]);
+    while (outarr[x]*1 >= mod) 
+    outarr[x] -= mod;
+  }
+  outarr = unique(outarr);
+  return outarr;
 }
 
 /**
@@ -42,20 +42,20 @@ function pcstr2pcs(mod, pstr) {
  * @author Johan Känngård, http://dev.kanngard.net
  */
 function unique(a) {
-	tmp = new Array(0);
-	for(i=0;i<a.length;i++){
-		if(!contains(tmp, a[i])){
-			tmp.length+=1;
-			tmp[tmp.length-1]=a[i];
-		}
-	}
-	return tmp;
+  tmp = new Array(0);
+  for(i=0;i<a.length;i++){
+    if(!contains(tmp, a[i])){
+      tmp.length+=1;
+      tmp[tmp.length-1]=a[i];
+    }
+  }
+  return tmp;
 }
 /**
  * Returns true if 's' is contained in the array 'a'
  * @author Johan Känngård, http://dev.kanngard.net
  */
 function contains(a, e) {
-	for(j=0;j<a.length;j++)if(a[j]==e)return true;
-	return false;
+  for(j=0;j<a.length;j++)if(a[j]==e)return true;
+  return false;
 }
--- js/plotset.js
+++ js/plotset.js
@@ -4,52 +4,44 @@ var pad = 20;
 var containersize = 2 * (radius + pad);
 
 function drawset(mod, pcs, destroy) {  
-	if (paper && destroy) clearPaper(paper);
-	paper = new Raphael(document.getElementById('canvas_container'), containersize, containersize);  
-	var circle = paper.circle(radius+pad, radius+pad, radius);
-	circle.attr({'stroke-width':3});
-	for (var i = 0; i < mod; i++)
-	{
-		var cos = Math.cos(i/mod*2*Math.PI);
-		var sin = Math.sin(i/mod*2*Math.PI);
-		var dot = paper.circle(radius*sin+radius+pad, -1*radius*cos+radius+pad, 4);
-		dot.attr({fill:"white"});
-	}
-	for (var i = pcs.length-1; i >= 0; i--)
-	{
-		var cos = Math.cos(pcs[i]/mod*2*Math.PI);
-		var sin = Math.sin(pcs[i]/mod*2*Math.PI);
-		var x = radius*sin+radius+pad;
-		var y = -1*radius*cos+radius+pad
-		if (i)
-		{
-			for (var j = i-1; j >= 0; j--)
-			{
-				var x2 = radius*Math.sin(pcs[j]/mod*2*Math.PI)+radius+pad;
-				var y2 = -1*radius*Math.cos(pcs[j]/mod*2*Math.PI)+radius+pad;
-				var ln = "M " + x + " " + y + " L " + x2 + " " + y2;
-				var line = paper.path(ln);
-				if (j==i-1 || (i==pcs.length-1 && j==0))
-				{
-					line.attr({
-						'stroke-width':3,
-						'stroke':'#666'
-					});
-				}
-				else
-				{
-					line.attr({
-						'stroke-width':2,
-						'stroke':'#ddd'
-					});				
-				}
-			}
-		}
-		var dot = paper.circle(x, y, 4);
-		dot.attr({fill:"black"});
-		dot.glow({'width':6});
-
-	}
+  if (paper && destroy) clearPaper(paper);
+  paper = new Raphael(document.getElementById('canvas_container'), containersize, containersize);  
+  var circle = paper.circle(radius+pad, radius+pad, radius);
+  circle.attr({'stroke-width':3});
+  for (var i = 0; i < mod; i++) {
+    var cos = Math.cos(i/mod*2*Math.PI);
+    var sin = Math.sin(i/mod*2*Math.PI);
+    var dot = paper.circle(radius*sin+radius+pad, -1*radius*cos+radius+pad, 4);
+    dot.attr({fill:"white"});
+  }
+  for (var i = pcs.length-1; i >= 0; i--) {
+    var cos = Math.cos(pcs[i]/mod*2*Math.PI);
+    var sin = Math.sin(pcs[i]/mod*2*Math.PI);
+    var x = radius*sin+radius+pad;
+    var y = -1*radius*cos+radius+pad
+    if (i) {
+      for (var j = i-1; j >= 0; j--) {
+        var x2 = radius*Math.sin(pcs[j]/mod*2*Math.PI)+radius+pad;
+        var y2 = -1*radius*Math.cos(pcs[j]/mod*2*Math.PI)+radius+pad;
+        var ln = "M " + x + " " + y + " L " + x2 + " " + y2;
+        var line = paper.path(ln);
+        if (j==i-1 || (i==pcs.length-1 && j==0)) {
+          line.attr({
+            'stroke-width':3,
+            'stroke':'#666'
+          });
+        } else {
+          line.attr({
+            'stroke-width':2,
+            'stroke':'#ddd'
+          });        
+        }
+      }
+    }
+    var dot = paper.circle(x, y, 4);
+    dot.attr({fill:"black"});
+    dot.glow({'width':6});
+  }
 }
 
 function clearPaper(paper){
