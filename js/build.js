fa24384e2413e3c794db208c6fa133620ed73836
Jeremiah Goyette <jsgoyette@gmail.com>
2012-05-31 01:32:32 -0400

    tabs to spaces

--- js/build.js
+++ js/build.js
@@ -1,11 +1,11 @@
 #!/usr/local/bin/node
 
 var fs = require('fs'),
-	uglify = require('uglify-js')
+  uglify = require('uglify-js')
 
 var pcJS = fs.readFileSync(__dirname + '/plot-pc-set.js').toString(),
-	p2pJS = fs.readFileSync(__dirname + '/pcstr2pcs.js').toString(),
-	copy = '/* Copyright (c) 2012 Jeremiah Goyette */'
+  p2pJS = fs.readFileSync(__dirname + '/pcstr2pcs.js').toString(),
+  copy = '/* Copyright (c) 2012 Jeremiah Goyette */'
 
 console.log('building and minifying...')
 buildFile(pcJS, 'plot-pc-set.min.js')
@@ -13,10 +13,10 @@ buildFile(p2pJS, 'pcstr2pcs.min.js')
 console.log('done')
 
 function buildFile(js, name) {
-	var ast = uglify.parser.parse(js)
-	ast = uglify.uglify.ast_mangle(ast)
-	ast = uglify.uglify.ast_squeeze(ast)
-	var minifiedJS = uglify.uglify.gen_code(ast)
-	fs.writeFile(__dirname + '/' + name, copy + '\n' + minifiedJS)
+  var ast = uglify.parser.parse(js)
+  ast = uglify.uglify.ast_mangle(ast)
+  ast = uglify.uglify.ast_squeeze(ast)
+  var minifiedJS = uglify.uglify.gen_code(ast)
+  fs.writeFile(__dirname + '/' + name, copy + '\n' + minifiedJS)
 }
 
